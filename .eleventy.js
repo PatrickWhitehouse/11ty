62ef83da213755ae60c173d656d6f188115cf084
Patrick Whitehouse <patrick1996@gmail.com>
2021-11-27 15:37:29 +0000

    restructure directories, content

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/styles.css");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/blog/img");
-  eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/js/app.js");
   eleventyConfig.addPlugin(pluginSEO, {
     title: "Patrick Whitehouse, front end developer based in Derbyshire, UK",
